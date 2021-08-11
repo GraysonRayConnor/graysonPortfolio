478a7a59a9151b2f2b89c4f96823c1118d129260
Grayson Connor <grayson.bolon@yahoo.com>
2021-08-10 19:12:05 -0500

    site redo v1

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+window.onload = () => {
+	const tab_switchers = document.querySelectorAll("[data-switcher]");
+	for (let i = 0; i < tab_switchers.length; i++) {
+		const tab_switcher = tab_switchers[i];
 
+		const page_id = tab_switcher.dataset.tab;
 
-function codeWars(){
-fetch("https://www.codewars.com/api/v1/users/TOOTS")
-.then(data => {
-    console.log(data.json());
-    });
+		tab_switcher.addEventListener("click", () => {
+			document.querySelector(".tab.is-active").classList.remove("is-active");
+
+			tab_switcher.classList.add("is-active");
+
+			SwitchPage(page_id);
+		});
+	}
+};
+
+function SwitchPage(page_id) {
+	const current_page = document.querySelector(".page.is-active");
+	current_page.classList.remove("is-active");
+	const next_page = document.querySelector(`.page[data-page="${page_id}"]`);
+	next_page.classList.add("is-active");
+}
+
+function codeWars() {
+	fetch("https://www.codewars.com/api/v1/users/TOOTS").then(data => {
+		console.log(data.json());
+	});
 }
 
 codeWars();
